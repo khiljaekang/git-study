38d04b84106f62a685824dee42b4d8235a6a3705
name명 <>
2020-05-19 19:03:20 +0900

    학습

--- python/python03_list.py
+++ python/python03_list.py
@@ -5,58 +5,58 @@
 a = [1,2,3,4,5,]
 b = [1,2,3,'a','b']
 
-print(b)
+print(b)                         #[1,2,3,'a','b']
 
-print(a[0] + a[3])
+print(a[0] + a[3])               # 5
 # print(b[0] + b[3])             #자료형이 달라서 오류
-print(type(a))
-print(a[-2])
-print(a[1:3])
+print(type(a))                   # list
+print(a[-2])                     # 4
+print(a[1:3])                    # [2, 3]
 
-a = [1, 2, 3, ['a', 'b', 'c']]
-print(a[1])
-print(a[-1])
-print(a[-1][1])
+a = [1, 2, 3, ['a', 'b', 'c']]   
+print(a[1])                      # 2
+print(a[-1])                     # ['a','b','c']
+print(a[-1][1])                  # b
 
 #1-2. 리스트 슬라이싱
 
 a = [1,2,3,4,5]
-print(a[:2])                     # 처음부터 2까지 나온다 
+print(a[:2])                     # [1, 2] 처음부터 2까지 나온다 
 
 #1-3. 리스트 더하기
 a = [1, 2, 3]
 b = [4, 5, 6]
 
-print(a + b)
+print(a + b)                     # [ 1,2,3,4,5,6]
 
 c = [7,8,9,10]
 
-print(a + c)
-print(a * 3)
+print(a + c)                     # [1,2,3,7,8,9,10]
+print(a * 3)                     # [1,2,3,1,2,3,1,2,3]
 
-# print(a[2] +'hi')               #타입에러 
+# print(a[2] +'hi')              #타입에러 
 
 print(str(a[2]) + 'hi')
 
 f = '5'
-# print(a[2] + f)                 #타입에러
+# print(a[2] + f)                #타입에러
 
-print((a[2]) + int(f))
+print((a[2]) + int(f))           # 8
 
 
 #리스트 관련 함수
-a = [1, 2, 3] 
-a.append(4)                     #.append = 추가하다
-print(a)
+a = [1, 2, 3]                
+a.append(4)                      #.append = 추가하다
+print(a)                         # [1,2,3,4]
 
-# a = a.append(5)               None   .append 할 때는 자기꺼에다 쓰면 안됨.
+# a = a.append(5)                None   .append 할 때는 자기꺼에다 쓰면 안됨.
 
 a = [1, 3, 4, 2]
-a.sort()                        #sort = 정렬하다
-print(a)
+a.sort()                         # sort = 정렬하다
+print(a)                         # [1,2,3,4]
 
-a.reverse()                     #reverse = 반대로하다
-print(a)                        # [4,3,2,1]
+a.reverse()                      #reverse = 반대로하다
+print(a)                         # [4,3,2,1]
 
 print(a.index(3))               #index = 색인  ==a[3]
 print(a.index(1))                              ==a[1]
