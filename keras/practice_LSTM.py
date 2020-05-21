c09e431fac21a932cc91200a0f2fde79b93c46f6
khiljaekang <64455713+khiljaekang@users.noreply.github.com>
2020-05-21 11:44:59 +0900

    Add files via upload

---
+++ keras/practice_LSTM.py
@@ -0,0 +1,16 @@
+#LSTM
+'''
+#LSTM의 입력은 항상 3D배열이다.
+LSTM 의 출력은 return_sequences 인수에 따라 2D 배열 또는 3D 배열 일 수 있습니다
+경우 return_sequence가 거짓이고, 출력은 2 차원 배열이다. (일괄 크기, 단위)
+경우 return_sequence가 True 인, 출력은 3D 배열입니다. (일괄 크기, 시간 단계, 단위)
+
+
+
+
+
+
+
+
+
+'''
