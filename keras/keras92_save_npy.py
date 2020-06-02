b0a366dd6abce487f6f870432c3551b00ab27e6d
khiljaekang <64455713+khiljaekang@users.noreply.github.com>
2020-06-02 09:16:15 +0900

    Add files via upload

---
+++ keras/keras92_save_npy.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import load_iris
+import numpy as np                            #numpy를 저장하고 불러오는 방법
+iris = load_iris()
+
+print(type(iris))
+
+x_data = iris.data
+y_data = iris.target
+
+print(type(x_data))
+print(type(y_data))
+
+# np.save('./data/iris_x.npy', arr = x_data)
+# np.save('./data/iris_y.npy', arr = y_data)
+
+x_data_load = np.load('./data/iris_x.npy')
+y_data_load = np.load('./data/iris_y.npy')
+
+print(type(x_data_load))
+print(type(y_data_load))
+print(x_data_load.shape)
+print(y_data_load.shape)
+
