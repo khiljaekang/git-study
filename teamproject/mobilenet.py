e394f0651a4fb89bac2613b4e2063eb10c517d5d
khiljaekang <pkwink@naver.com>
2020-07-27 11:50:04 +0900

    학습

--- teamproject/mobilenet.py
+++ teamproject/mobilenet.py
@@ -1,17 +1,18 @@
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Flatten
-from keras.applications import DenseNet169, DenseNet201, NASNetMobile, MobileNet,Xception
-from keras.optimizers import Adam
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.applications import DenseNet169, DenseNet201, NASNetMobile, MobileNet,Xception,ResNet101V2,InceptionV3, ResNet50
+from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 import time
+from tensorflow.keras.optimizers import Adam, RMSprop, SGD, Adadelta, Adagrad, Nadam
 
 start = time.time()
 
 # load data
-x = np.load('D:/teamproject/data/dog_img.npy')
-y = np.load('D:/teamproject/data/dog_label.npy')
+x = np.load('D:/teamproject/data/face_image_total.npy')
+y = np.load('D:/teamproject/data/face_label_total.npy')
 
 print(x.shape) # (7160, 112, 112, 3)
 print(y.shape) # (7160, 11)
@@ -22,22 +23,23 @@ print('======== data load ========')
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state = 66)
 
 # model
-takemodel = MobileNet(include_top=False, input_shape = (112, 112, 3),)
-# takemodel.trainable=False
+takemodel = MobileNet(include_top=False, input_shape = (128, 128, 3),)
+takemodel.trainable=False
 model = Sequential()
 model.add(takemodel)
 model.add(Flatten())
-# model.add(Dense(300,activation="relu"))
+model.add(Dense(300,activation="relu"))
 model.add(Dense(12, activation = 'softmax'))
+optimizer = SGD(lr=0.001)
 
 model.summary()
 
-cp = ModelCheckpoint('D:/teamproject/checkpoint/best_MobileNet_128.hdf5', monitor = 'val_loss',
+cp = ModelCheckpoint('D:/teamproject/checkpoint/mobilenet_128.hdf5', monitor = 'val_loss',
                     save_best_only = True, save_weights_only = False)
 es = EarlyStopping(monitor= 'val_loss', patience = 25, verbose =1)
 
 #3. compile, fit
-model.compile(optimizer = Adam(1e-4), loss = 'categorical_crossentropy', metrics = ['acc'])                             
+model.compile(optimizer = optimizer, loss = 'sparse_categorical_crossentropy', metrics = ['acc'])                             
 hist = model.fit(x_train, y_train, epochs = 100, batch_size = 128, verbose = 1, 
                  validation_split =0.2 , shuffle = True, callbacks = [es, cp])
 
@@ -45,7 +47,7 @@ hist = model.fit(x_train, y_train, epochs = 100, batch_size = 128, verbose = 1,
 #     weights = layer.get_weights()
 #     print(weights)
 #4. evaluate
-loss_acc = model.evaluate(x_test, y_test, batch_size = 128)
+loss_acc = model.evaluate(x_test, y_test, batch_size = 32)
 print('loss_acc: ' ,loss_acc)
 
 end = time.time()
@@ -71,7 +73,7 @@ plt.plot(hist.history['val_acc'], marker = '^', c = 'cyan', label = 'val_acc')
 plt.grid()
 plt.title('acc')
 plt.xlabel('epochs')
-plt.ylabel('loss')
+plt.ylabel('acc')
 plt.legend()
 
 plt.show()
