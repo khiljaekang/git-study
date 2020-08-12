bc7d2d9549ae725e54ffd7b39c12a0c2cf1171e8
khiljaekang <pkwink@naver.com>
2020-08-12 09:12:03 +0900

    학습

--- teamproject/numpy_hdf5.py
+++ teamproject/numpy_hdf5.py
@@ -2,21 +2,20 @@ import cv2
 import numpy as np
 import h5py
 
-# image
-
 dataset = np.load('D:/data/face_image_total.npy')
 
 def resize(dataset, w, h):
 
-    f = h5py.File('D:/data/image_hdf5.hdf5','a')
+    f = h5py.File('D:/data/image_hdf5.hdf5','a')    # 'w' : write = 없으면 생성. 있으면 리셋 후 재생성
+                                                    # 'r' : read  = 읽기 전용
+                                                    # 'a' :       = 없으면 생성, 있으면 갱신  
 
-    imageset = f.create_dataset('imageset%d'%(w), (dataset.shape[0], h, w, 3)) #, dtype='float64')
+    imageset = f.create_dataset('imageset%d'%(w), (dataset.shape[0], w, h, 3)) #, dtype='float64')
 
     for i in range(len(dataset)):
         print(i)
         img = dataset[i]
-        img_umat = cv2.UMat(img)            ## opencv opencl-gpu ro dol ril su it ge sok dodo PPal la jim
-                
+        img_umat = cv2.UMat(img)                 
         img_result = cv2.resize(img_umat, (w, h), interpolation = cv2.INTER_LINEAR)
         img_result = cv2.UMat.get(img_result)       ## UMat.get = numpy ro ba QUim
 
@@ -33,6 +32,6 @@ label = np.load('D:/data/face_label_total.npy')
 w = 1024
 
 with h5py.File('D:/data/image_hdf5.hdf5','a') as f:
-    f.create_dataset('label%d'%(w), data = label)
-    print(f['label%d'%(w)][:].shape[0])
+    f.create_dataset('label', data = label)
+    print(f['label'][:].shape[0])
 '''
