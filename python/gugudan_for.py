a7a8fa3112c26ebfc7264b22a43d76eb51e03113
khiljaekang <64455713+khiljaekang@users.noreply.github.com>
2021-03-16 21:42:28 +0900

    for문 구구단

---
+++ python/gugudan_for.py
@@ -0,0 +1,10 @@
+n = 2
+
+for i in range(1,10):
+    print(n, '*', i, '=', n*i)
+    
+################################
+
+for dan in range(2,10):
+    for i in range(1,10):
+        print(dan, '*', i, '=',dan*i )
